fc3927744cab7d573d42f88ec8c38f2b648e2eec
Cyrillo <matheus@negocie.online>
2016-12-29 16:14:18 -0200

    Aprendendo sobre o prompt

--- ChatBot/Dialogs/SimpleLUISDialog.cs
+++ ChatBot/Dialogs/SimpleLUISDialog.cs
@@ -2,6 +2,7 @@
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,55 +11,70 @@ using System.Web;
 
 namespace ChatBot.Dialogs
 {
-    [LuisModel("e0357190-4455-4506-8764-055b7e04a674", "a91e3e2044be4be99c291c54a153f3a6")]
+    [LuisModel("91eaac2c-7694-4cf2-be62-d45a29f5418d", "82e8297cfa944fa7b40e1cde76ca8d1a")]
     [Serializable]
     public class SimpleLUISDialog : LuisDialog<object>
     {
 
 
-        [LuisIntent("Pagar")]
+        [LuisIntent("AlterarDados")]
         //sending the payment by email
-        public async Task Payment(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        public async Task Alterar(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
         {
             // logic to retrieve the current payment info..
-            var email = "test@email.com";
+            var email = "example@email.com";
 
-            PromptDialog.Confirm(context, AfterEmailConfirmation, $"Is it {email} your current email?");
+            
+
+          //  PromptDialog.Confirm(context, AfterEmailConfirmation, $"Enviaremos o boleto para este e-mail: {email}, você gostaria de trocá-lo?");
         }
 
-        private async Task AfterEmailConfirmation(IDialogContext context, IAwaitable<bool> result)
+        //private async Task AfterEmailConfirmation(IDialogContext context, IAwaitable<bool> result)
+        //{
+        //    try
+        //    {
+        //        var response = await result;
+
+        //        // if the way to store the payment email is the same as the one used to store the email when going through the ChangeInfo intent, then you can use the same After... method; otherwise create a new one
+        //        PromptDialog.Text(context, AfterEmailProvided, "What's your current email?");
+        //    }
+        //    catch
+        //    {
+        //        // here handle your errors in case the user doesn't not provide an email
+        //    }
+
+        //    context.Wait(this.MessageReceived);
+        //}
+
+        //private async Task AfterEmailProvided(IDialogContext context, IAwaitable<string> result)
+        //{
+        //    try
+        //    {
+        //        var email = await result;
+
+        //        // logic to store your email...
+        //    }
+        //    catch
+        //    {
+        //        // here handle your errors in case the user doesn't not provide an email
+        //    }
+
+        //    context.Wait(this.MessageReceived);
+        //}
+
+        [LuisIntent("Pagar")]
+        //sending the payment by email
+        public async Task Pagar(IDialogContext context, LuisResult result)
         {
-            try
-            {
-                var response = await result;
-
-                // if the way to store the payment email is the same as the one used to store the email when going through the ChangeInfo intent, then you can use the same After... method; otherwise create a new one
-                PromptDialog.Text(context, AfterEmailProvided, "What's your current email?");
-            }
-            catch
-            {
-                // here handle your errors in case the user doesn't not provide an email
-            }
-
-            context.Wait(this.MessageReceived);
+            // logic to retrieve the current payment info..
+            var valor = "200,00";
+            var linhaDigitavel = "592030592-3239";
+            var data = "05/01/2016";
+            var nome = "Michele";  
+              
+            await context.PostAsync($"Prezado {nome}, sua fatura está no valor de R${valor}, com a data de vencimento em {data}. Linha digitável: {linhaDigitavel} ");
+            context.Wait(MessageReceived);
         }
-
-        private async Task AfterEmailProvided(IDialogContext context, IAwaitable<string> result)
-        {
-            try
-            {
-                var email = await result;
-
-                // logic to store your email...
-            }
-            catch
-            {
-                // here handle your errors in case the user doesn't not provide an email
-            }
-
-            context.Wait(this.MessageReceived);
-        }
-
 
         [LuisIntent("")]
         [LuisIntent("None")]
--- ChatBot/Services/Luis.cs
+++ ChatBot/Services/Luis.cs
@@ -17,7 +17,7 @@ namespace ChatBot.Services
             {
                 const string authKey = "03f03812f59c4a6aa5f57c8077abaaa3";
 
-                var url = $"https://api.projectoxford.ai/luis/v2.0/apps/ae6f9d6f-63d5-4bf2-9b3f-d691f2ff13b8?subscription-key={authKey}&q={message}&verbose=true";
+                var url = $"https://api.projectoxford.ai/luis/v2.0/apps/ae6f9d6f-63d5-4bf2-9b3f-d691f2ff13b8?subscription-key={authKey}&q={message}";
 
                 var jsonString = new System.Net.WebClient().DownloadString(url);
                 JsonObject informacaoJSON = JsonConvert.DeserializeObject<JsonObject>(jsonString);
